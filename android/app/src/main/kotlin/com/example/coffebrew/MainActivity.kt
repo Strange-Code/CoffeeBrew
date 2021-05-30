1be827fdaea8378503f158ffae25ce1bb61a3a72
Renato Ponce Barrera <rponce@Mac-mini-de-Renato.local>
2021-05-29 20:28:50 -0500

    First commit

---
+++ android/app/src/main/kotlin/com/example/coffebrew/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.coffebrew
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/mccafe.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Mccafe extends StatefulWidget {
+  @override
+  _MccafeState createState() => _MccafeState();
+}
+
+class _MccafeState extends State<Mccafe> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text('McCafe'));
+  }
+}
